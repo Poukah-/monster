48b013d40443508aa46c18a58a049c638d9bc99d
Poukah <petersson.john@gmail.com>
2019-09-10 01:39:29 +0200

    Updates

---
+++ precache-manifest.60087f7fc4ebedd7df97996bc3d5b699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303f9289c54340eb1a8627cecefbd1da",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "786c719707c4853530f5",
+    "url": "/monster/static/css/main.bee47808.chunk.css"
+  },
+  {
+    "revision": "2ab373573a2da60aeefa",
+    "url": "/monster/static/js/2.b3f2c5c4.chunk.js"
+  },
+  {
+    "revision": "786c719707c4853530f5",
+    "url": "/monster/static/js/main.d8b93aef.chunk.js"
+  },
+  {
+    "revision": "007385cd75acb6210c2e",
+    "url": "/monster/static/js/runtime~main.416336db.js"
+  }
+]);
